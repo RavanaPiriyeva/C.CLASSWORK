aaffc4b70a937305ecf1368b07be70ba06d98026
RavanaPiriyeva <100703061+RavanaPiriyeva@users.noreply.github.com>
2022-03-31 15:49:14 +0400

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adi daxil et:");
+            string name = Console.ReadLine();
+            Console.WriteLine("emaili daxil edin :");
+            string emaile = Console.ReadLine();
+            Console.WriteLine("sifreni daxil et:");
+            string pasword = Console.ReadLine();
+
+            User user = new User(name, emaile) ;
+
+            user.ShowInfo();
+        }
+    }
+}
---
+++ ConsoleApp8/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class User: IAccount
+    {
+
+        public User(string fullname , string email)
+        {
+            this.FullName = fullname;
+            this.Email = email;
+        }
+        public string FullName { get; set; }
+        
+
+        public string Email { get; set; }
+        private string _password;
+        public string Password { get { return _password; } 
+        set
+            {
+                if (PasswordChecker(value) == true)
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public bool PasswordChecker(string pasword)
+        {
+           if (pasword.Length >= 8)
+            {
+                bool check1 = false;
+                bool check2 = false;
+                bool check3 = false;
+                foreach (var item in pasword)
+                {
+                    if (char.IsDigit(item))
+                    {
+                       check1 = true;
+                    }
+                    if (char.IsLetter(item))
+                    {
+                         check2 = true;
+                    }
+                    if (char.IsUpper(item))
+                    {
+                        check3 = true;
+                    }
+
+
+                }
+                if (check1==true && check2==true && check3==true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Fullname:{FullName} Email:{Email} Pasword:{Password}");
+        }
+    }
+}
