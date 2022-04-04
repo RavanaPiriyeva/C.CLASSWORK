d9c99ff67c740369df4adab52eef5f2ba107d023
RavanaPiriyeva <100703061+RavanaPiriyeva@users.noreply.github.com>
2022-04-04 16:25:35 +0400

    Add files via upload

---
+++ ConsoleApp14/Check.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp14
+{
+    class Check
+    {
+        public static bool CustomContains(string sentence, string word)
+        {
+            var result = sentence.Split(' ');
+                foreach( var item in result) { 
+                if (item == word)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+
+        }
+        public static bool CustomContains(string word, char letter)
+        {
+           
+            foreach (var item in word)
+            {
+                if (item == letter)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string test = "salam necesen";
+            string word = "salam";
+            Console.WriteLine(Check.CustomContains(test, word));
+            Console.WriteLine(Check.CustomContains(word, 'x'));
+        }
+    }
+}
