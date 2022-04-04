924495f6982cc0d57c287bfc51e42112e24a3de5
RavanaPiriyeva <100703061+RavanaPiriyeva@users.noreply.github.com>
2022-04-04 15:59:16 +0400

    Add files via upload

---
+++ New folder (2)/ConsoleApp1/Check.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Check
+    {
+       public static bool IsPrime(int num)
+        {
+           for (int i = 2; i<num; i++)
+            {
+                if(num%i==0 )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+       public  static bool IsPowOfTwo(int num)
+        {
+           
+            if (num % 2 == 0) { 
+                while (num / 2 > 1)
+                {
+                num = num / 2;
+                }
+            }
+            if (num == 2)
+            {
+                return true;
+            }
+            return false;
+         
+
+
+        }
+    }
+}
---
+++ New folder (2)/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check test = new Check();
+            Console.WriteLine(Check.IsPrime(3));
+            Console.WriteLine(Check.IsPowOfTwo(33));
+        }
+    }
+}
